d8fa504f32a09dec70ba7918b2fcc25cd34e86f3
sorea.no <sorae.no@thingsflow.com>
2022-04-26 23:24:44 +0900

    build: add compose dependencies

---
+++ app/src/main/java/com/example/composeintegration/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.composeintegration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        val cv = findViewById<ComposeView>(R.id.cv_compose)
+//        cv.setContent {
+//            Greeting()
+//        }
+    }
+
+//    @Composable
+//    private fun Greeting() {
+//        Text(
+//            text = "Hello Compose!",
+//            style = MaterialTheme.typography.h5,
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(horizontal = 10.dp)
+//                .wrapContentWidth(Alignment.CenterHorizontally)
+//        )
+//    }
+}
