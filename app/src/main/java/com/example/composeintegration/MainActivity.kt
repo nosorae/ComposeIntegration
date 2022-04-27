13775f4ce8f280884064c846e4385c2a7d68aa53
sorea.no <sorae.no@thingsflow.com>
2022-04-27 14:46:42 +0900

    feat: Column, Row 사용

--- app/src/main/java/com/example/composeintegration/MainActivity.kt
+++ app/src/main/java/com/example/composeintegration/MainActivity.kt
@@ -1,32 +1,71 @@
 package com.example.composeintegration
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.unit.dp
+import java.lang.Thread.sleep
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
         val cv = findViewById<ComposeView>(R.id.cv_compose)
         cv.setContent {
-            Greeting()
+            MessageCard(msg = Message("Author name", "Body"))
         }
+
     }
 
     @Composable
     private fun Greeting() {
-        Text(text = "Hello World!, Compose")
+        Text(text = "Hello World!, Compose", color = Color.White)
+    }
+
+    data class Message(val author: String, val body: String)
+
+    @Composable
+    fun MessageCard(msg: Message) {
+
+        // Add padding around our message
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_foreground),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    // Set image size to 40 dp
+                    .size(40.dp)
+                    // Clip image to be shaped as a circle
+                    .clip(CircleShape)
+            )
+
+            // Add a horizontal space between the image and the column
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(text = msg.author, color = Color.White)
+                // Add a vertical space between the author and message texts
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = msg.body, color = Color.White)
+            }
+        }
     }
 }
